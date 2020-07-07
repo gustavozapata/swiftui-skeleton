eea2d9ed1fab85249c496cc4ae6a04f660a8346c
Gustavo Zapata <tavordie@hotmail.com>
2020-07-07 01:07:57 +0100

    before starting drawing and animation

--- swiftUIApp/ContactDetail.swift
+++ swiftUIApp/ContactDetail.swift
@@ -9,22 +9,42 @@
 import SwiftUI
 
 struct ContactDetail: View {
+    @EnvironmentObject var userData: UserData
     var contact: Contact
+    
+    var contactIndex: Int {
+        userData.contacts.firstIndex(where: {$0.id == contact.id})!
+    }
     
     var body: some View {
         VStack{
             MapView(coordinate: contact.locationCoordinate)
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
-
+            
             CircleImage(image: contact.image)
                 .offset(x: 0, y: -130)
                 .padding(.bottom, -130)
-
+            
             VStack(alignment: .leading) {
-                Text(contact.name)
-                    .font(.title)
-
+                HStack {
+                    Text(contact.name)
+                        .font(.title)
+                    
+                    Button(action: {
+                        self.userData.contacts[self.contactIndex].isFavorite.toggle()
+                    }) {
+                        if self.userData.contacts[self.contactIndex].isFavorite {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color.yellow)
+                        } else {
+                            Image(systemName: "star")
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                }
+                
+                
                 HStack(alignment: .top) {
                     Text(contact.park)
                         .font(.subheadline)
--- swiftUIApp/ContactList.swift
+++ swiftUIApp/ContactList.swift
@@ -9,23 +9,43 @@
 import SwiftUI
 
 struct ContactList: View {
+    @EnvironmentObject var userData: UserData
+    
     var body: some View {
         NavigationView {
-            List(contactsData) { contact in
-                NavigationLink(destination: ContactDetail(contact: contact)){
-                    ContactRow(contact: contact)
+            List {
+                Toggle(isOn: $userData.showFavoritesOnly){
+                    Text("Favorites only")
                 }
+                
+                ForEach(userData.contacts) { contact in
+                    if !self.userData.showFavoritesOnly || contact.isFavorite {
+                        NavigationLink(destination: ContactDetail(contact: contact)){
+                            ContactRow(contact: contact)
+                        }
+                    }
+                }
+                
             }
             .navigationBarTitle(Text("Contacts"))
         }
     }
 }
 
+
 struct ContactList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone 11 Pro", "iPad Pro (9.7-inch)"], id: \.self){ deviceName in
-            ContactList()
-            .previewDevice(PreviewDevice(rawValue: deviceName))
-        }
+        ContactList()
+            .environmentObject(UserData())
+            .previewDevice("iPhone 11 Pro")
     }
 }
+
+//struct ContactList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ForEach(["iPhone 11 Pro", "iPad Pro (9.7-inch)"], id: \.self){ deviceName in
+//            ContactList()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//        }
+//    }
+//}
